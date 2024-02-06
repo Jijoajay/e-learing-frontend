ffae28c3b87f2c3d4b4abc0cf9ad616077565bf4
ajay <ajay@ajays-MacBook-Air.local>
2024-02-06 16:36:23 +0530

    new changes made

--- src/components/api/flashapi.js
+++ src/components/api/flashapi.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "http://127.0.0.1:4500",
+    baseURL: "https://elearning-backend-jk11.onrender.com",
     withCredentials:true,
     headers:{
         'Content-Type': 'application/json',  // set header so server knows we are sending json data
